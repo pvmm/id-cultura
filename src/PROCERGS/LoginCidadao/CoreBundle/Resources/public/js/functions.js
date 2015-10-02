9d9d73292d4b8e37a404d879e4904a35d18fb147
fernao <fernao@riseup.net>
2015-10-02 12:15:18 -0300

    arquivos do novo layout

---
+++ src/PROCERGS/LoginCidadao/CoreBundle/Resources/public/js/functions.js
@@ -0,0 +1,55 @@
+// If JavaScript is enabled remove 'no-js' class and give 'js' class
+jQuery('html').removeClass('no-js').addClass('js');
+
+// Add .osx class to html if on Os/x
+if ( navigator.appVersion.indexOf("Mac")!=-1 ) 
+	jQuery('html').addClass('osx');
+
+// When DOM is fully loaded
+jQuery(document).ready(function($) {
+
+/* --------------------------------------------------------	
+	 External Links
+   --------------------------------------------------------	*/	
+
+	(function() {
+	    $(window).load(function() {
+			$('a[rel=external]').attr('target','_blank');	
+		});                                            
+	})();  
+
+/* --------------------------------------------------------	
+	 Responsive Navigation
+   --------------------------------------------------------	*/	
+	
+	(function() {
+
+		var $mainNav    = $('.navbar .nav'),
+			responsiveNav = '';
+
+		// Responsive nav
+		$mainNav.find('li').each(function() {
+			var $this   = $(this),
+				$link = $this.children('a'),
+				depth   = $this.parents('ul').length - 1,
+				indent  = '';
+
+			if( depth ) {
+				while( depth > 0 ) {
+					indent += ' - ';
+					depth--;
+				}
+			}
+
+			if ($link.text())
+				responsiveNav += '<option ' + ($this.hasClass('active') ? 'selected="selected"':'') + ' value="' + $link.attr('href') + '">' + indent + ' ' + $link.text() + '</option>';
+
+		}).end().after('<select class="nav-responsive">' + responsiveNav + '</select>');
+
+		$('.nav-responsive').on('change', function() {
+			window.location = $(this).val();
+		});
+			
+	})();      
+
+});
